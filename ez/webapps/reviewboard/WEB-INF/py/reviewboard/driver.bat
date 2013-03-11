310318b5bd860c9d3d70b1b57c9b1da95a513eaf
zoei <zoei@163.com>
2013-03-11 17:58:54 +0800

    add login method

---
+++ ez/webapps/reviewboard/WEB-INF/py/reviewboard/driver.bat
@@ -0,0 +1,3 @@
+python driver.py sunshaojun 121223 submit
+pause
+python driver.py caozhenyu a22718895 submit
--- ez/webapps/reviewboard/WEB-INF/py/reviewboard/driver.py
+++ ez/webapps/reviewboard/WEB-INF/py/reviewboard/driver.py
@@ -10,31 +10,52 @@ import csv, collections
 import http.client
 import httplib2
 
-#usrname = sys.args[1]
-#pwd = sys.args[2]
-#optiontype = sys.args[3]
-option = 'submit'
+username = sys.argv[1]
+passwd = sys.argv[2]
+option = sys.argv[3]
+#option = 'submit'
 ROOT_DIR = os.getcwd()
-TEMP_DIR = os.path.sep.join((ROOT_DIR, "data", "temp"))
-OUTPUT_DIR = os.path.sep.join((ROOT_DIR, "data"))
-OUTPUT_FILE = os.path.sep.join((OUTPUT_DIR, "%s.csv" % "result"))
 
 Record = collections.namedtuple("Record", "id,summary,submiter,posttime,updatetime")
 recordheader = Record("ID", "内容", "提交者", "内容", "备注")
 records = []
-http = httplib2.Http('.cache')
 outputhtml = ''
 
-def downloadPage(url):
-    res,ctt = http.request(url, 'GET')
+http = httplib2.Http('.cache')
+HOST = 'http://172.26.184.150'
+headers = {'Content-type': 'application/x-www-form-urlencoded'}
+
+login_url = 'http://172.26.184.150/reviews/account/login/'
+login_data = {'username':username ,'password':passwd, 'next_page':'/reviews/dashboard/'}
+
+submit_url = 'http://172.26.184.150/reviews/api/review-requests/%s/'
+submit_data = {'api_format':'json', 'status':'submitted'}
+
+review_url = 'http://172.26.184.150/reviews/api/review-requests/%s/reviews/'
+review_data = {'api_format':'json', 'ship_it':'1', 'body_top':'', 'body_bottom':'', 'public':'1'}
+
+ship_url = 'http://172.26.184.150/reviews/api/review-requests/%s/reviews/'
+ship_data = {'api_format':'json', 'ship_it':'1', 'body_top':'', 'body_bottom':'', 'public':'1'}
+
+def login():
+    url = "http://172.26.184.150/reviews/account/login/"
+    res,ctt = apirequest(url)
+    headers['Cookie'] = res['set-cookie']
+
+    res,ctt = http.request(url, 'POST', headers=headers, body=urllib.parse.urlencode(login_data))
+    headers['Cookie'] = res['set-cookie']
+    print('login success')
+
+def downloadPage(url,optiontype):
+    res,ctt = apirequest(url)
     print('%s [%s]' % (url,res.status))
 
     restext = ctt.decode('utf_8')
 
-    tempfile = "%s/httplib2_result.html" % (TEMP_DIR)
+    tempfile = "httplib2_result.html"
 
     save2file(tempfile, restext)
-    return parsehtml(restext)
+    return parsehtml(restext,optiontype)
 
 def save2file(filepath, content):
     f = open(filepath, "w", encoding="utf8")
@@ -51,40 +72,50 @@ def parsehtml(html,optiontype):
             summary = tr('a')[0].text
             newrecord = Record(reviewid,summary,'','','')
             records.append(newrecord)
-            #print('%s [%s]%s ...' %(optiontype,reviewid,summary))
-            #eval('%s(%s)' % (optiontype ,reviewid))
         return True
     except Exception as e:
         print(e)
         return False
 
 def submit(reviewid):
-    url = ('%s%d%s' % ('http://172.26.184.150/reviews/api/review-requests/' , reviewid , '/?api_format=json'))
+    url = ('%s%s%s' % ('http://172.26.184.150/reviews/api/review-requests/' , reviewid , '/'))
+    submit_data = {'api_format':'json', 'status':'submitted'}
+    res,ctt = http.request(url, 'PUT', headers=headers, body=urllib.parse.urlencode(submit_data))
     apirequest(url)
+
+def submitall(data):
+    print('开始提交')
+    for r in data:
+        print('%s [%s]%s ...' %('提交',r.id,r.summary))
+        submit(r.id)
+    print('全部提交成功')
 
 def apirequest(url):
     try:
-        print(url)
-        res,ctt = http.request(url, 'GET')
+        print('url: ' + url)
+        res,ctt = http.request(url, 'GET', headers=headers)
         print(res.status)
+        return res,ctt
     except Exception as e:
         print('NG:%s' % e)
+        return None,None
 
 def parsebyfile(optiontype):
     f = open("list.htm", "r", encoding="utf_8")
     html = f.read()
     f.close()
     parsehtml(html,optiontype)
-    
+
 def parsebyurl(optiontype):
     start = 1
     end = 1
+    login()
     for i in range(start, end+ 1):
-        url = "http://s.weibo.com/weibo/"
+        url = "http://172.26.184.150/reviews/dashboard/?view=outgoing"
         if not downloadPage(url,optiontype):
             print("not more content")
             break
-    print('提交成功')
+    print('下载成功')
 
 def write2html(data):
     #soup = BeautifulSoup()
@@ -105,9 +136,9 @@ def write2html(data):
     f.close()
 
 print("----------处理开始----------")
-#parsebyurl(option)
-parsebyfile(option)
-
+parsebyurl(option)
+#parsebyfile(option)
 write2html(records)
+submitall(records)
 print("----------处理结束----------")
 
